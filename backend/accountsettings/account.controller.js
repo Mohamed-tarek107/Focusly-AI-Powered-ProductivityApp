b9a961f0f58277b0c11deac937016cc58735c8b5
Mohamed <mohamedtarekk710@gmail.com>
2025-12-07 05:06:43 +0200

    Account settings backend Done (delete account + edit account info + change password

--- backend/accountsettings/account.controller.js
+++ backend/accountsettings/account.controller.js
@@ -1,1 +1,99 @@
 const db = require("../db.js");
+const bcrypt = require("bcryptjs");
+
+const editInfo = async (req,res) => {
+    const  id  = req.user.user_id
+
+    const { fullname, email, fname, lname, phone_number } = req.body
+
+    try {
+        const updates = {}
+
+        if(fullname) updates.fullname = fullname;
+        if(email) updates.email = email;
+        if(fname) updates.fname = fname;
+        if(lname) updates.lname = lname;
+        if(phone_number) updates.phone_number = phone_number;
+        
+
+        if (Object.keys(updates).length === 0) {
+            return res.status(400).json({ message: "No update data provided." });
+        }
+
+
+        const query = "UPDATE users SET " + Object.keys(updates).map((key) => `${key} = ?`).join(', ') +
+        " WHERE id = ?"
+
+        const values = [...Object.values(updates), id]
+        const [result] = await db.execute(query, values)
+
+        if(result.affectedRows === 0){
+                return res.status(404).json({ message: "User not found"})
+            }
+
+            const [updatedTask] = await db.execute(
+            "SELECT * FROM users WHERE id = ?", [ id ]
+            );
+
+            return res.json(updatedTask[0])
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({ message: "Server Error"})
+        }
+}
+
+const deleteAccount = async (req,res) => {
+    const id = req.user.user_id;
+
+    try {
+        await db.execute("DELETE FROM refreshtokens WHERE user_id = ?", [id])
+        await db.execute("DELETE FROM notes WHERE user_id = ?", [id])
+        await db.execute("DELETE FROM tasks WHERE user_id = ?", [id])
+        const [result] = await db.execute("DELETE FROM users WHERE id = ?", [id])
+    
+    
+    if (result.affectedRows === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        return res.json({ message: "Account Deleted Successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server Error" });
+    }
+    
+}
+
+const changePass = async (req,res) => {
+    const  id  = req.user.user_id
+    const {currentPass, NewPass, ConfirmPass} = req.body;
+
+    if(!currentPass){
+        return res.status(400).json({message: " Need current password "});
+    }
+    if(!NewPass || !ConfirmPass){
+        return res.status(400).json({ message: "Provide New pass and its confirmation" });
+    }
+
+    if(NewPass !== ConfirmPass){
+        return res.status(400).json({ message: "new Password dont match the confirmation" });
+    }
+
+    try {
+    const [[user]] = await db.execute("SELECT password_hashed FROM users WHERE id = ?", [id])
+    const verify = await bcrypt.compare(currentPass, user.password_hashed)
+
+    if(!verify) return res.status(400).json({ message: "current password is not correct" });
+    
+
+    const hashedNew = await bcrypt.hash(NewPass, 10)
+    await db.execute("UPDATE users SET password_hashed = ? WHERE id = ?",[hashedNew, id])
+
+    return res.status(200).json({ message: "Password Changed Successfully", })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server Error" });
+    }
+}
+
+module.exports = { changePass, editInfo, deleteAccount }
--- backend/accountsettings/account.routes.js
+++ backend/accountsettings/account.routes.js
@@ -1,3 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 const { ensureAuthenticated } = require("../Auth/auth.controller");
+const { changePass, editInfo, deleteAccount } = require("./account.controller")
+
+
+const Validation = [
+        body('phone_number')
+            .optional({ checkFalsy: true })
+            .trim()
+            .matches(/^[0-9+\-\s()]+$/)
+            .withMessage('Please provide a valid phone number'),
+
+        body('password')
+            .isLength({ min: 8 })
+            .withMessage('Password must be at least 8 characters long')
+            .matches(/[A-Z]/)
+            .withMessage('Password must contain at least one uppercase letter')
+            .matches(/[0-9]/)
+            .withMessage('Password must contain at least one number'),
+        
+        body('confirmpass')
+            .custom((value, { req }) => value === req.body.password)
+            .withMessage('Passwords do not match'),
+            
+        body('fname')
+            .notEmpty()
+            .withMessage('First name is required')
+            .trim()
+            .isLength({ min: 2, max: 20 })
+            .withMessage('First name must be between 2 and 20 characters')
+            .matches(/^[a-zA-Z\s'-]+$/)
+            .withMessage('First name can only contain letters'),
+            
+        body('lname')
+            .notEmpty()
+            .withMessage('Last name is required')
+            .trim()
+            .isLength({ min: 2, max: 20 })
+            .withMessage('Last name must be between 2 and 20 characters')
+            .matches(/^[a-zA-Z\s'-]+$/)
+            .withMessage('Last name can only contain letterss'),
+        
+        body('email')
+            .optional()
+            .isEmail()
+            .withMessage('Invalid email format'),
+]
+
+
+router.patch("/editAccount",ensureAuthenticated, Validation, editInfo)
+router.patch("/changePassword",ensureAuthenticated, Validation, changePass)
+router.delete("/deleteAccount",ensureAuthenticated, deleteAccount)
