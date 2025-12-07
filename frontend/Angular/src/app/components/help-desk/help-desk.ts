47aa0bcd4c70bd85412ae2c3a95cc66ecaf7e52e
Mohamed <mohamedtarekk710@gmail.com>
2025-12-08 00:06:58 +0200

    Fixes in helpdesk component

--- frontend/Angular/src/app/components/help-desk/help-desk.ts
+++ frontend/Angular/src/app/components/help-desk/help-desk.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Rating } from '../../services/AccountSettings/account-settings';
+import { Authservice } from '../../services/AuthService/auth';
 
 @Component({
   selector: 'app-help-desk',
@@ -10,13 +11,31 @@ import { Rating } from '../../services/AccountSettings/account-settings';
   templateUrl: './help-desk.html',
   styleUrl: './help-desk.css'
 })
-export class HelpDesk {
-  currentUser: string = 'User Name';
+export class HelpDesk implements OnInit{
+  currentUser: string = '';
   rating: number = 0;
   feedback: string = '';
   ratingText: string = '';
   ratingClass: string = '';
+  errorMessage: string = ''
 
+  constructor(private user: Authservice, private cdr: ChangeDetectorRef){}
+
+  
+  ngOnInit() {
+    this.user.current().subscribe({
+      next: (data) => {
+          console.log('User data received:', data); // Debug 
+          this.currentUser = data.fullname
+          this.cdr.detectChanges();
+      },
+      error: (err) => {
+          console.error('Error fetching user data:', err); // Debug
+          this.errorMessage = 'Failed to fetch user data';
+          this.cdr.detectChanges();
+      }
+    });
+  }
   setRating(star: number): void {
     this.rating = star;
 
