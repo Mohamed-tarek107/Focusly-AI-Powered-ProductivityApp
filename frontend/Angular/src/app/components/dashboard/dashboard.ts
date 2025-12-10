387e7b90a12c944d707254791f71ded4f3e8651e
Mohamed <mohamedtarekk710@gmail.com>
2025-12-10 18:33:24 +0200

    Intialized calcualte progress dynammically

--- frontend/Angular/src/app/components/dashboard/dashboard.ts
+++ frontend/Angular/src/app/components/dashboard/dashboard.ts
@@ -1,8 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
+import { Authservice } from '../../services/AuthService/auth';
+import { AccountSettingsService } from '../../services/AccountSettings/account-settings';
+import { Priority, Status } from '../../services/TasksService/tasks-service';
+
+
+
+interface Task {
+  task_id?: number
+  id?: number;
+  title?: string;
+  task_description?: string;
+  priority?: Priority;
+  task_status?: Status;
+  assigned_to?: string;
+  start_date?: string;
+  due_date?: string;
+  is_done?: boolean;
+  user_id?: number;
+}
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,40 +31,48 @@ import { Navbar } from '../navbar/navbar';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
-  currentUser = 'Mohamed Tarek';
-  progressPercentage = 89;
-  currentMonth = 'March 2024';
+export class Dashboard implements OnInit{
 
+  constructor(private user: Authservice, private cdr: ChangeDetectorRef, private setting: AccountSettingsService){}
+  currentUser = '';
+  progressPercentage = 0;
+  currentMonth = '';
+  tasks: Task[] = [];
   dayHeaders = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
   calendarDates = [0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
   upcomingTasks = [
     {
-      timeLeft: '10 Min Left',
-      title: 'Brainstorming',
-      description: 'Brainstorming with team on slothy app',
-      priority: 'Medium',
+      timeLeft: '',
+      title: '',
+      description: '',
+      priority: '',
       secondaryTag: null,
-      date: '23 Mar 2024',
-      time: '12:45 pm'
-    },
-    {
-      timeLeft: '58 Min Left',
-      title: 'Re-branding Discussion',
-      description: 'Discussion on re-branding of dermo Brand',
-      priority: 'High',
-      secondaryTag: 'Medium',
-      date: '23 Mar 2024',
-      time: '1:30 pm'
+      date: '',
+      time: ''
     }
   ];
 
   taskList = [
-    { name: 'Schedule post Dusk&Dawn', completed: true },
-    { name: 'Design post for Holi', completed: true },
-    { name: 'Brainstorming new project', completed: false },
-    { name: 'Re-Branding Discussion', completed: false },
-    { name: 'User Research', completed: false }
+    { name: '', completed: true }
   ];
+  ngOnInit(): void {
+    this.user.current().subscribe({
+      next: (data) => {
+        this.currentUser = data.fullname;
+      }
+    })
+  }
+
+
+  calcuateProgress(){
+    const total = this.taskList.length;
+    const completed = this.taskList.filter(task => task.completed).length;
+
+    if(total > 0){
+      this.progressPercentage = Math.round(( completed / total) * 100) 
+    }else{
+      this.progressPercentage = 0;
+    }
+  }
 }
