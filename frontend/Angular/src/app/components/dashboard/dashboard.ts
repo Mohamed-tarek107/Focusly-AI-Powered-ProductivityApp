79307f70407708358398046ae04fcb167b9f8e19
Mohamed <mohamedtarekk710@gmail.com>
2025-12-10 21:48:54 +0200

    UI changes in dashboard + added loadtasks service to dashboard

--- frontend/Angular/src/app/components/dashboard/dashboard.ts
+++ frontend/Angular/src/app/components/dashboard/dashboard.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 import { Navbar } from '../navbar/navbar';
 import { Authservice } from '../../services/AuthService/auth';
 import { AccountSettingsService } from '../../services/AccountSettings/account-settings';
-import { Priority, Status } from '../../services/TasksService/tasks-service';
+import { Priority, Status, TasksService } from '../../services/TasksService/tasks-service';
 
 
 
@@ -27,52 +27,84 @@ interface Task {
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [Navbar, CommonModule, FormsModule],
+  imports: [Navbar, CommonModule, FormsModule, RouterLink],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class Dashboard implements OnInit{
 
-  constructor(private user: Authservice, private cdr: ChangeDetectorRef, private setting: AccountSettingsService){}
+  constructor(private user: Authservice, private cdr: ChangeDetectorRef, private setting: AccountSettingsService, private task: TasksService){}
   currentUser = '';
   progressPercentage = 0;
   currentMonth = '';
-  tasks: Task[] = [];
   dayHeaders = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
   calendarDates = [0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-  upcomingTasks = [
+  tasks = [
     {
       timeLeft: '',
       title: '',
-      description: '',
+      task_description: '',
       priority: '',
       secondaryTag: null,
-      date: '',
-      time: ''
+      due_date: '',
+      time: '',
+      is_done: false
     }
   ];
 
-  taskList = [
-    { name: '', completed: true }
+  Todos = [
+    { name: '', is_done: true }
   ];
+
+
+
   ngOnInit(): void {
     this.user.current().subscribe({
       next: (data) => {
         this.currentUser = data.fullname;
       }
     })
+    this.calcuateProgress()
   }
 
-
-  calcuateProgress(){
-    const total = this.taskList.length;
-    const completed = this.taskList.filter(task => task.completed).length;
-
+calcuateProgress(){
+    const total = this.tasks.length + this.Todos.length;
+    const completedtasks = this.tasks.filter(task => task.is_done).length;
+    const completeTodos = this.Todos.filter(task => task.is_done).length;
+    const completed = completeTodos + completedtasks
+    
+    
     if(total > 0){
       this.progressPercentage = Math.round(( completed / total) * 100) 
     }else{
       this.progressPercentage = 0;
     }
   }
+
+
+  loadTasks() {
+    this.task.getAllTasks().subscribe({
+      next: (res: any) => {
+        if(res && res.length > 0){
+          this.tasks = res.map((task: any) => ({
+            title: task.title,
+            task_description: task.task_description,
+            priority: task.priority,
+            start_date: task.start_date,
+            task_status: task.task_status,
+            assigned_to: task.assigned_to,
+            due_date: task.due_date,
+            is_done: task.is_done == 1,
+          }))
+          console.log('✅ Tasks loaded:', this.tasks);
+          this.cdr.detectChanges();
+        }
+      },
+      error: (err) => {
+        console.error('❌ Error fetching tasks:', err);
+      }
+    })
+  }
+
 }
