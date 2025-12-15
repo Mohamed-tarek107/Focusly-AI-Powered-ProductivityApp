893b488c6545faa69c57499a6d63b492a23b5cfd
Mohamed <mohamedtarekk710@gmail.com>
2025-12-15 13:56:08 +0200

    Added guard to routes

--- frontend/Angular/src/app/app.routes.ts
+++ frontend/Angular/src/app/app.routes.ts
@@ -8,17 +8,17 @@ import { AiAssistant } from './components/AiAssistant/ai-assistant';
 import { AccountSettings } from './components/account-settings/account-settings';
 import { HelpDesk } from './components/help-desk/help-desk';
 import { NotfoundPage } from './components/notfound-page/notfound-page';
-
+import { authGuard } from './guards/auth.guard'
 
 export const routes: Routes = [
     { path: '', redirectTo: 'Login', pathMatch: 'full' },
     { path: 'Login', component: Login },
     { path: 'Register', component: Register },
     { path: 'Forget', component: ForgetPage},
-    { path: 'Dashboard', component: Dashboard},
-    { path: 'Tasks', component: TasksComponent},
-    { path: 'AiAssistant', component: AiAssistant},
-    { path: 'settings', component: AccountSettings},
-    { path: 'help', component: HelpDesk},
+    { path: 'Dashboard', component: Dashboard, canActivate: [authGuard]},
+    { path: 'Tasks', component: TasksComponent, canActivate: [authGuard]},
+    { path: 'AiAssistant', component: AiAssistant, canActivate: [authGuard]},
+    { path: 'settings', component: AccountSettings, canActivate: [authGuard]},
+    { path: 'help', component: HelpDesk, canActivate: [authGuard]},
     { path: '**', component: NotfoundPage }
 ];
